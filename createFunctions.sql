74957ccc1ab80f8a24def4c873ba476f2bbea264
PeterBadzhakov <44850335+PeterBadzhakov@users.noreply.github.com>
2020-06-04 18:08:02 +0300

    Changes

--- createFunctions.sql
+++ createFunctions.sql
@@ -3,11 +3,32 @@
 -- function: fix car
 set schema PETAR_NIKO_PROTECTED;
 
-CREATE FUNCTION utod(epoch_time int)
-RETURNS DATE
+CREATE FUNCTION utt(start_time int)
+RETURNS timestamp
 BEGIN ATOMIC
-    DECLARE start DATE;
-    SET start = ADD_SECONDS(DATE '1970-01-01', epoch_time);
+    DECLARE epoch timestamp default TIMESTAMP('1970-01-01-00.00.00.000000');
+    DECLARE result timestamp;
+
+    SET result = epoch + start_time seconds;
+    RETURN result;
 END;
 
+CREATE FUNCTION ttu(start_timestamp timestamp)
+RETURNS int
+RETURN 0;
+/*
+CREATE FUNCTION ttu(start_timestamp timestamp)
+RETURNS int
+BEGIN ATOMIC
+DECLARE d int;
+SET d = DAY(start_timestamp)*24*60*60;
+DECLARE m int;
+SET m = MONTH(start_timestamp)*30*24*60*60;
+DECLARE y int;
+SET y = YEAR(start_timestamp - 1970)*365*24*60*60;
+
+DECLARE res int default 0;
+SET res = d + m + y;
+END;
+ */
 
